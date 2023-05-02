ea8455de271e4960df18c7abbb7388525026cb57
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-02 23:02:06 +0200

    Test principal four equality cases

---
+++ app/utils.test.js
@@ -0,0 +1,44 @@
+import strictEquals from "./utils";
+
+describe("Given a compare function", () => {
+  describe("When it receives 1 and 1", () => {
+    test("Then it should return true", () => {
+      const valueA = 1;
+      const valueB = 1;
+      const equality = strictEquals(valueA, valueB);
+      const isEqual = true;
+
+      expect(equality).toBe(isEqual);
+    });
+  });
+  describe("When it receives 1 and '1'", () => {
+    test("Then it should return false", () => {
+      const valueA = 1;
+      const valueB = "1";
+      const equality = strictEquals(valueA, valueB);
+      const isEqual = false;
+
+      expect(equality).toBe(isEqual);
+    });
+  });
+  describe("When it receives true and false", () => {
+    test("Then it should return false", () => {
+      const valueA = true;
+      const valueB = false;
+      const equality = strictEquals(valueA, valueB);
+      const isEqual = false;
+
+      expect(equality).toBe(isEqual);
+    });
+  });
+  describe("When it receives true and false", () => {
+    test("Then it should return false", () => {
+      const valueA = false;
+      const valueB = false;
+      const equality = strictEquals(valueA, valueB);
+      const isEqual = true;
+
+      expect(equality).toBe(isEqual);
+    });
+  });
+});
